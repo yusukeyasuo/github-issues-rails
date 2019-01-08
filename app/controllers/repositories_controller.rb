7eba54b1afd18cefd1dcb404dbd549f2ee400dc4
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-01-09 01:32:51 +0900

    リポジトリの閲覧、登録、削除

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,2 +1,29 @@
 class RepositoriesController < ApplicationController
+  def index
+    @repositories = Repository.all
+  end
+
+  def new
+    @repository = Repository.new
+  end
+
+  def create
+    @repository = Repository.new(post_params)
+    if @repository.save
+      redirect_to repositories_path, notice: '保存しました'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @repository = Repository.find_by(id: params[:id])
+    @repository.destroy if @repository.present?
+    redirect_to repositories_path
+  end
+
+  private
+    def post_params
+      params.require(:repository).permit(:owner, :repository)
+    end
 end
