49a35d116367829d34935f5702d27aa1704d6077
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-01-09 03:26:25 +0900

    Issueの取得、一覧の表示

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,6 +3,12 @@ class RepositoriesController < ApplicationController
     @repositories = Repository.all
   end
 
+  def show
+    @repository = Repository.find_by(id: params[:id])
+    redirect_to repositories_path unless @repository.present?
+    @issues = @repository.issues.order(id: :desc).limit(5)
+  end
+
   def new
     @repository = Repository.new
   end
@@ -20,6 +26,31 @@ class RepositoriesController < ApplicationController
     @repository = Repository.find_by(id: params[:id])
     @repository.destroy if @repository.present?
     redirect_to repositories_path
+  end
+
+  def reload
+    @repository = Repository.find_by(id: params[:id])
+    redirect_to repositories_path unless @repository.present?
+    csv = Github.get_issues("https://api.github.com/repos/#{@repository.owner}/#{@repository.repository}/issues")
+    issues = []
+    issue_strs = csv.split("\n")
+    issue_strs.reverse.each do |issue_str|
+      i = issue_str.split(',')
+      if i.size == 3
+        issues << Issue.new(
+          repository_id: params[:id],
+          title: i[0],
+          body: i[1],
+          html_url: i[2]
+        )
+      end
+    end
+    issues.each do |i|
+      unless Issue.find_by(html_url: i.html_url)
+        i.save
+      end
+    end
+    redirect_to repository_path(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "repositories#index"
   resources :repositories
+  resources :issues
+
+  get 'repositories/:id/issues', to: 'repositories#reload', as: :repository_issues
 end
