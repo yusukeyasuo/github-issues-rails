1a327a5a924a82fbae6c64812a05c54291f4c878
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-01-09 08:45:51 +0900

    初期リポジトリをseedで登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[
+  { owner: 'rails', repository: 'rails' },
+].each do|repository_set|
+  repository = Repository.find_or_create_by(owner: repository_set[:owner], repository: repository_set[:repository])
+  repository.save
+end
+
